8772e343f998cd86213df43ec526df5070747873
Suban Purushothmakumar <Suban Purushothmakumar>
2017-02-01 22:02:24 +0000

    Challenge complete

---
+++ CS-ASP_012_Challenge/CS-ASP_012_Challenge/CS-ASP_012_Challenge/Default.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CS_ASP_012_Challenge.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Your Note Taking Preference</div>
+        <p>
+            &nbsp;</p>
+        <asp:RadioButton ID="pencilRadioButton" runat="server" GroupName="NoteTakingPreference" Text="Pencil" />
+        <br />
+        <asp:RadioButton ID="penRadioButton" runat="server" GroupName="NoteTakingPreference" Text="Pen" />
+        <br />
+        <asp:RadioButton ID="phoneRadioButton" runat="server" GroupName="NoteTakingPreference" Text="Phone" />
+        <br />
+        <asp:RadioButton ID="tabletRadioButton" runat="server" GroupName="NoteTakingPreference" Text="Tablet" />
+        <p>
+            &nbsp;</p>
+        <asp:Button ID="okButton" runat="server" OnClick="okButton_Click" Text="OK" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        <br />
+        <br />
+        <asp:Image ID="resultImage" runat="server" />
+    </form>
+</body>
+</html>
---
+++ CS-ASP_012_Challenge/CS-ASP_012_Challenge/CS-ASP_012_Challenge/Default.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CS_ASP_012_Challenge;
+
+namespace CS_ASP_012_Challenge
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            string prePendString = "You selected a ";
+
+            if (pencilRadioButton.Checked)
+            {
+                resultLabel.Text = prePendString + pencilRadioButton.Text;
+                resultImage.ImageUrl = "~/Images/pencil.png";
+            }
+            else if (penRadioButton.Checked)
+            {
+                resultLabel.Text = prePendString + penRadioButton.Text;
+                resultImage.ImageUrl = "~/Images/pen.png";
+            }
+            else if (phoneRadioButton.Checked)
+            {
+                resultLabel.Text = prePendString + phoneRadioButton.Text;
+                resultImage.ImageUrl = "~/Images/phone.png";
+            }
+            else if (tabletRadioButton.Checked)
+            {
+                resultLabel.Text = prePendString + tabletRadioButton.Text;
+                resultImage.ImageUrl = "~/Images/tablet.png";
+            }
+            else
+            {
+                resultLabel.Text = "Please select an option";
+            }
+
+        }
+    }
+}
